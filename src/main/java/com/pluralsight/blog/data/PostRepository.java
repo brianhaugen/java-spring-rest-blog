2d1682b47752011eb626ba33aa6a45496b261a2f
brianhaugen <brian.haugen@gmail.com>
2022-07-26 12:08:45 -0700

    broken first part
    I know this is broken, but the directions are not giving the expected output.

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -2,28 +2,31 @@ package com.pluralsight.blog.data;
 
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+
+
 @Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
+public abstract class PostRepository implements JpaRepository<Post, Long> {
+
+
+    final PostRepository postRepository;
+
+    @Autowired
+    public PostRepository(PostRepository pr){
+        this.postRepository = pr;
     }
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title) {
+        return List<Post>;
     }
 }
+
